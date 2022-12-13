3ea297edb771ac2589ceccb1161dc85e9758a5f2
RasikaRavi <rasikaravi2020@gmail.com>
2022-12-13 20:22:03 +0530

    first commit

---
+++ src/components/DropDown.js
@@ -0,0 +1,57 @@
+import React from 'react';
+// import ColorChanger from './ColorChanger';
+
+function DropDown(props){
+    const options = [
+        {
+          label: "Lime",
+          value: "lime"
+        },
+    
+        { label: "Lavender", value: "lavender" },
+    
+        {
+          label: "Crimson",
+          value: "crimson"
+        },
+    
+        { 
+           label: "Darkblue", 
+           value: "darkblue"
+        },
+    
+        { 
+           label: "Teal", 
+           value: "teal" 
+        }
+      ];
+
+    const [value, setValue] = React.useState("lime");
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+      };
+
+    return (
+        <div>
+          <select value={value} onChange={handleChange}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+    
+          <div
+            style={{
+              marginTop: 100,
+              marginLeft: 200,
+              width: 300,
+              height: 200,
+              backgroundColor: value
+            }}
+          />
+        </div>
+      );
+    };
+
+
+export default DropDown;
